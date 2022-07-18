7dbd71225ad513e61bc9f789cf4ad85d870b0882
Cesar Jr.Fontalvo <juniorfontalvo5@gmail.com>
2022-07-18 17:44:10 -0500

    Fix: Vista inscripcion al reto

--- src/modules/landing/postulationChallenge/views/CreatePostulation.tsx
+++ src/modules/landing/postulationChallenge/views/CreatePostulation.tsx
@@ -19,44 +19,11 @@ const CreatePostulation = () => {
             <div className="d-flex flex-row mb-3 col-md-12">
               <h5 className="my-5">¡Genial! estas a punto de postularte al reto</h5>
             </div>
-            <div className="col-10">
-              {/* <Card title=" Datos del postulante" actions={[]}>
-                <FormPostulation innerRef={form_ref} onSubmit={addPostulation} />
-              </Card> */}
-{/* ---------------------------------------------------------------------------------------------- */}
-              {/* <div className="site-card-wrapper">
-                <div className="row">
-                  <div className="col">
-                  <Card actions={[]}>
-                    <DocumentChallenge innerRef={form_ref} onSubmit={addPostulation}/>
-                  </Card></div>
-                  <div className="col">
-                  <Card actions={[]}>
-                    <DocumentChallenge innerRef={form_ref} onSubmit={addPostulation}/>
-                  </Card></div>
-                  <div className="col">
-                  <Card actions={[]}>
-                    <DocumentChallenge innerRef={form_ref} onSubmit={addPostulation}/>
-                  </Card></div>
-                </div>
-
-                <div className="row ">
-                  <div className="col-4">
-                  <Card actions={[]}>
-                    <DocumentChallenge innerRef={form_ref} onSubmit={addPostulation}/>
-                  </Card></div>
-                  <div className="col-4">
-                  <Card actions={[]}>
-                    <DocumentChallenge innerRef={form_ref} onSubmit={addPostulation}/>
-                  </Card></div>
-                 
-                </div>
-
-              </div> */}
-  {/* ------------------------------------------------------------------------------------------------- */}
+            <div className="">
               <Card title=" Datos del postulante" actions={[]}>
-                <FormLocation  innerRef={form_ref} onSubmit={addPostulation}/>
+                <FormPostulation innerRef={form_ref} onSubmit={addPostulation} />
               </Card>
+
             </div>
 
           </div>
