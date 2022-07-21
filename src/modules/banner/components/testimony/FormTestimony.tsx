7f05e3bea2b41f3cc1d5f8b55e14152ef4803e87
Cesar Jr.Fontalvo <juniorfontalvo5@gmail.com>
2022-07-21 12:59:46 -0500

    fix: resetForm a formulario testimony

--- src/modules/banner/components/testimony/FormTestimony.tsx
+++ src/modules/banner/components/testimony/FormTestimony.tsx
@@ -31,6 +31,8 @@ const FormTestimony: FC<TestimonyFormPros> = ({
 
   const submit = (values: any, form: any) => {
     onSubmit(values);
+    form.setSubmitting(false);
+    form.resetForm();
   };
 
   return (
